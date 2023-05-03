12963c2cd3911f72889b8e330a6bed1676ef6f60
stilCyber <karnozavr666@ya.ru>
2023-05-03 23:31:19 +0600

    lesson 3 full

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 8888;
 const parserMiddleware = (0, body_parser_1.default)({});
 app.use(parserMiddleware);
-const products = [{ id: 1, title: 'cherry' }, { id: 2, title: 'lemon' }];
+const products = [{ id: 1, title: 'cherry' }, { id: 2, title: 'lemon' }, { id: 3, title: 'orange' }, { id: 4, title: 'banana' },];
 app.get('/', (req, res) => {
     res.send('Hi');
 });
@@ -19,13 +19,42 @@ app.get('/products', (req, res) => {
 app.get('/items', (req, res) => {
     res.send('New field');
 });
-app.post('/products', (req, res) => {
-    const newProduct = {
-        id: +(new Date()),
-        title: req.body.title
-    };
-    products.push(newProduct);
-    res.send(newProduct);
+// app.post('/products', (req: Request, res: Response) => {
+//     const newProduct = {
+//         id: +(new Date()),
+//         title: req.body.title
+//     }
+//     products.push(newProduct)
+//     res.send(newProduct)
+// })
+app.delete('/products/:id', (req, res) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1);
+            res.send(204);
+            return;
+        }
+    }
+    res.send(404);
+});
+app.get('/products/:id', (req, res) => {
+    let oneProduct = products.find(f => f.id === +req.params.id);
+    if (oneProduct) {
+        res.send(oneProduct);
+    }
+    else {
+        res.send(404);
+    }
+});
+app.put('/products/:id', (req, res) => {
+    let product = products.find(p => p.id === +req.params.id);
+    if (product) {
+        product.title = req.body.title;
+        res.send(req.body.title);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.listen(port, () => {
     console.log(`Example app listening on port: ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8888
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
 
-const products = [{id: 1, title: 'cherry'}, {id: 2, title: 'lemon'}]
+const products = [{id: 1, title: 'cherry'}, {id: 2, title: 'lemon'}, {id: 3, title: 'orange'}, {id: 4, title: 'banana'},]
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hi')
@@ -21,14 +21,48 @@ app.get('/items', (req: Request, res: Response) => {
     res.send('New field')
 })
 
-app.post('/products', (req: Request, res: Response) => {
-    const newProduct = {
-        id: +(new Date()),
-        title: req.body.title
+// app.post('/products', (req: Request, res: Response) => {
+//     const newProduct = {
+//         id: +(new Date()),
+//         title: req.body.title
+//     }
+//     products.push(newProduct)
+//     res.send(newProduct)
+// })
+
+
+app.delete('/products/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < products.length; i++) {
+        if(products[i].id === +req.params.id) {
+            products.splice(i, 1)
+            res.send(204)
+            return
+        }
     }
-    products.push(newProduct)
-    res.send(newProduct)
+    res.send(404)
 })
+
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    let oneProduct = products.find(f => f.id === +req.params.id)
+    if(oneProduct) {
+        res.send(oneProduct)
+    } else {
+        res.send(404)
+    }
+})
+
+
+app.put('/products/:id', (req: Request, res: Response) => {
+    let product = products.find(p => p.id === +req.params.id)
+    if(product) {
+       product.title = req.body.title
+       res.send(req.body.title)
+    } else {
+       res.send(404)
+    }
+    
+ })
 
 app.listen(port, () => {
     console.log(`Example app listening on port: ${port}`)
